98814ad1a1748cae99688b280e89dd5a12252512
Daniel Silva <up201806524@fe.up.pt>
2020-10-07 15:54:31 +0100

    alarm and macros

---
+++ FirstProject/macros.h
@@ -0,0 +1,16 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define BAUDRATE        B38400
+#define MODEMDEVICE     "/dev/ttyS1"
+#define _POSIX_SOURCE   1 /* POSIX compliant source */
+#define FALSE           0
+#define TRUE            1
+#define FLAG            0x7E
+#define SEND_ADDRESS    0x03
+#define SET_COMMAND     0x03
+#define UA_ANSWER       0x07
+#define SET_FRAME_SIZE  5
+
+
+#endif /*MACROS_H*/
