193cef96fa2dc74321d57d54299fa7a6a8d1ac40
Daniel Silva <up201806524@fe.up.pt>
2020-11-10 18:53:26 +0000

    a

--- FirstProject/src/macros.h
+++ FirstProject/src/macros.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define BAUDRATE        B38400
+#define BAUDRATE        B9600
 #define MODEMDEVICE     "/dev/ttyS1"
 #define _POSIX_SOURCE   1 /* POSIX compliant source */
 #define _XOPEN_SOURCE   700
