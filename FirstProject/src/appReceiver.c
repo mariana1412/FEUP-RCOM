ca1325853ca0bab08415615cb5eceb9e98b5f183
Daniel Silva <up201806524@fe.up.pt>
2020-11-10 12:18:20 +0000

    byte

--- FirstProject/src/appReceiver.c
+++ FirstProject/src/appReceiver.c
@@ -194,7 +194,7 @@ int initFile(){
 int parseInfo(unsigned char *info, int size) {
 
     unsigned char byte = info[0];
-    printf("packet byte: ")
+    printf("packet byte: %d", byte);
 
     switch (byte)
     {
